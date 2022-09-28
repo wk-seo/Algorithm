bcb7bc3363d3699dd603bcc109173c116abf596d
Seo <108641066+wk-seo@users.noreply.github.com>
2022-09-28 16:55:35 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.25 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.cs
@@ -0,0 +1,29 @@
+public class Solution 
+{
+    public int solution(long num) 
+    {
+        int answer = 0;
+
+        while (num != 1)
+        {
+            if (num % 2 == 0)
+            {
+                num /= 2;
+                answer++;
+            }
+
+            else
+            { 
+                num = (num * 3) + 1;
+                answer++;
+            }
+        }
+
+        if (answer >= 500)
+        {
+            answer = -1;
+        }
+
+        return answer;
+    }
+}
