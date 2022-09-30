01a848ecc11dece65634948e0d225dce11c79ca1
Seo <108641066+wk-seo@users.noreply.github.com>
2022-09-30 12:00:13 +0900

    Update 숫자 문자열과 영단어.cs

--- 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cs
+++ 프로그래머스/lv1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cs
@@ -3,7 +3,11 @@ using System;
 public class Solution {
     public int solution(string s)
     {
-        string [] words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        //단어를 받음
+        string [] words = 
+        {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        //s에 단어가들어있는지 한별
+        //string형 숫자로 치환함
         for(int i = 0; i < words.Length; i++)
         {
             if(s.Contains(words[i]))
@@ -12,6 +16,7 @@ public class Solution {
             }
         }
         
+        //치환 완료한 string을 int로 변환
         int answer = int.Parse(s);
         return answer;
     }
