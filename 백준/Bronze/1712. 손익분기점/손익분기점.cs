7c81f3ebb1c7bc00ec433b75f9b7f85f78715ef4
Seo <108641066+wk-seo@users.noreply.github.com>
2022-10-27 09:47:15 +0900

    [Bronze II] Title: 손익분기점, Time: 60 ms, Memory: 5460 KB -BaekjoonHub

--- 백준/Bronze/1712. 손익분기점/손익분기점.cs
+++ 백준/Bronze/1712. 손익분기점/손익분기점.cs
@@ -1,8 +1,18 @@
+using System.Text;
+//A=고정비용, B=가변비용(1대 생산), A+B는 전체 비용
+//C=노트북 가격
+
 long[] inputs = Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
-if (inputs[2] <= inputs[1])
-{
-    Console.WriteLine(-1);
-    return;
+
+if(inputs[1]>=inputs[2]) Console.WriteLine("-1");
+else{
+//int i = 0; //판매량
+    //inputs[0]+inputs[1]*i = inputs[2]*i
+    //(inputs[0]/(inputs[2]-inputs[1]))+1=i
+    //inputs[1]*i-inputs[2]*i >= inputs[0]
+    //(inputs[1]-inputs[2])*i >= inputs[0]
+    //i >= inputs[0]/(inputs[1]-inputs[2]);
+    
+    Console.WriteLine((inputs[0]/(inputs[2]-inputs[1]))+1);
 }
 
-Console.WriteLine(inputs[0]/(inputs[2]-inputs[1]) +1);
