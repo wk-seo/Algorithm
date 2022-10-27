fcf9eebdcc469b611c88ce4ae0ea8474b1e53c23
Seo <108641066+wk-seo@users.noreply.github.com>
2022-10-27 10:38:00 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 60 ms, Memory: 5468 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cs
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.cs
@@ -1,11 +1,12 @@
-       int n = Int32.Parse(Console.ReadLine());
-       Console.WriteLine(F(n));            
+int n = int.Parse(Console.ReadLine());
 
-    int F(int n)
-    {
-        int result = 0;
-        if (n == 0) result = 0;
-        else if (n == 1) result = 1;
-        else result = F(n - 1) + F(n - 2);
-        return result;
+int F(int n){
+    int result = 0;
+    if(n == 0) return result;
+    else if(n == 1) return result+1;
+    else{
+        return F(n-1)+F(n-2);
     }
+}
+
+Console.WriteLine(F(n).ToString());
