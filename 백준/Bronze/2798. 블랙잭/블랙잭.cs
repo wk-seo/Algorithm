a5bea79973f1350d890bcf4d63991b11f30c0047
Seo <108641066+wk-seo@users.noreply.github.com>
2022-11-01 10:52:37 +0900

    [Bronze II] Title: 블랙잭, Time: 68 ms, Memory: 7296 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.cs
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cs
@@ -1,18 +1,15 @@
 using System.Collections.Generic;
 
 int[] inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-int[] cards = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
-
-List<int> tries = new List<int>();
-for (int i = 0; i < cards.Length - 2; i++)
-{
-    for (int j = i + 1; j < cards.Length - 1; j++)
-    {
-        for (int k = j + 1; k < cards.Length; k++)
-        {
-            if (inputs[1] - (cards[i] + cards[j] + cards[k]) >= 0) 
-            {tries.Add(cards[i]+cards[j]+cards[k]);}
+int[] nums = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+List<int> sums  = new List<int>();
+for(int i = 0; i<inputs[0]-2; i++){
+    for(int j = i+1; j<inputs[0]-1; j++){
+        for(int k = j+1; k<inputs[0]; k++){
+            if(nums[i]+nums[j]+nums[k]<=inputs[1]) 
+                sums.Add(nums[i]+nums[j]+nums[k]);
         }
     }
 }
-Console.WriteLine(tries.Max());
+
+Console.WriteLine(sums.Max());
