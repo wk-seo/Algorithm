69cdbb92c634f974585de74ecc38ef64707bb79d
Seo <108641066+wk-seo@users.noreply.github.com>
2022-11-08 16:33:33 +0900

    [Silver IV] Title: 수 찾기, Time: 180 ms, Memory: 26580 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.cs
+++ 백준/Silver/1920. 수 찾기/수 찾기.cs
@@ -1,31 +1,28 @@
 using System.Text;
 
+int n = int.Parse(Console.ReadLine());
+int[] arrA = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int m = int.Parse(Console.ReadLine());
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 StringBuilder sb = new StringBuilder();
-int totalC = int.Parse(Console.ReadLine());
-string[] str = Console.ReadLine().Split();
-int[] criteria = new int[str.Length];
-int totalS = int.Parse(Console.ReadLine());
-string[] search = Console.ReadLine().Split();
 
-for(int i = 0; i < str.Length; i++){
-    criteria[i] = int.Parse(str[i]);
-}
-
-Array.Sort(criteria);
-
-for(int i = 0; i<totalS; i++){
-    int low = 0;
-    int high = criteria.Length-1;
-    bool found = false;
-    
-    while(low <= high && !found){
-        int mid = (low+high)/2;
-        
-        if(criteria[mid]==int.Parse(search[i])) found = true;
-        else if(criteria[mid]>int.Parse(search[i])) high = mid -1;
-        else low = mid+1;
-    }
-    sb.Append(found?"1\n":"0\n");
+Array.Sort(arrA);
+for (int i = 0; i < m; i++)
+{
+    int temp = arr[i];
+    sb.AppendLine(BinarySearch(0, n - 1, arr[i]).ToString());
 }
 
 Console.WriteLine(sb.ToString());
+
+int BinarySearch(int start, int end, int num)
+{
+    int mid = (start + end) / 2;
+    if (arrA[mid] == num) return 1;
+    else if (num < arrA[start] || num > arrA[end]) return 0;
+    else
+    {
+        if (arrA[mid] > num) return BinarySearch(start, mid, num);
+        else return BinarySearch(mid + 1, end, num);
+    }
+}
