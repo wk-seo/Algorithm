a627dea80dc69b018045d9c9a3e6d6233d64432a
Seo <108641066+wk-seo@users.noreply.github.com>
2022-12-13 14:40:01 +0900

    [Silver IV] Title: 제로, Time: 80 ms, Memory: 9304 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.cs
+++ 백준/Silver/10773. 제로/제로.cs
@@ -1,20 +1,15 @@
-using System.Collections.Generic;
-
-int k = int.Parse(Console.ReadLine());
 List<int> list = new List<int>();
+int k = int.Parse(Console.ReadLine());
 for (int i = 0; i < k; i++)
 {
-    list.Add(int.Parse(Console.ReadLine()));
-    if (list[i] == 0)
+    int x = int.Parse(Console.ReadLine());
+    if (x == 0)
     {
-        for (int j = list.Count - 1; j >= 0; j--)
-        {
-            if (list[j] != 0)
-            {
-                list[j] = 0;
-                break;
-            }
-        }
-    }             
+        list.RemoveAt(list.Count-1);
+    }
+    else
+    {
+        list.Add(x);
+    }
 }
 Console.WriteLine(list.Sum());
