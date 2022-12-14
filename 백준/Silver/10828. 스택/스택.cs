213ae0e5f2aa3cfb05fc8a975f130719f3c979f2
Seo <108641066+wk-seo@users.noreply.github.com>
2022-12-14 09:55:48 +0900

    [Silver IV] Title: 스택, Time: 88 ms, Memory: 7656 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.cs
+++ 백준/Silver/10828. 스택/스택.cs
@@ -1,71 +1,37 @@
 using System.Collections.Generic;
 using System.Text;
 
-public class MainApp{
-    
-    static List<string> sstack = new List<string>();
-    static StringBuilder sb = new StringBuilder();
-    
-    public static void Main(string[] args)
-    {
-        int n = int.Parse(Console.ReadLine());
-        for (int i = 0; i < n; i++)
-        {
-            sb.Clear();
-            string[] str = Console.ReadLine().Split();
-            
-            switch (str[0])
-            {
-                case "push":
-                    push(str[1]);
-                    break;
-                case "pop":
-                    pop();
-                    break;
-                case "size":
-                    size();
-                    break;
-                case "empty":
-                    empty();
-                    break;
-                case "top":
-                    top();
-                    break;
-            }
+StringBuilder sb = new StringBuilder();
+
+List<int> stack = new List<int>();
+int n = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < n; i++){
+    string[] temp = Console.ReadLine().Split();
+    if(temp.Length == 2){
+        stack.Add(int.Parse(temp[1]));
+    }
+    else{
+        switch(temp[0]){
+            case "pop":
+                if(stack.Count==0)
+                    sb.AppendLine("-1");
+                else{
+                    sb.AppendLine(stack[stack.Count-1].ToString());
+                    stack.RemoveAt(stack.Count-1);
+                }
+                break;
+            case "size":
+                sb.AppendLine(stack.Count.ToString());
+                break;
+            case "empty":
+                sb.AppendLine(stack.Count==0?"1":"0");
+                break;
+            case "top":
+                sb.AppendLine(stack.Count==0?"-1":stack[stack.Count-1].ToString());
+                break;
         }
     }
-    
-    public static void push(string x)
-    {
-        sstack.Add(x);
-    }
-
-    public static void pop()
-    {
-        sb.Append(sstack.Count == 0 ? "-1" : sstack[sstack.Count - 1]);
-        Console.WriteLine(sb);
-        if(sstack.Count!=0) 
-            sstack.RemoveAt(sstack.Count - 1);
-    }
-
-    public static void size()
-    {
-        sb.Append(sstack.Count);
-        Console.WriteLine(sb);
-    }
-
-    public static void empty()
-    {
-        sb.Append(sstack.Count == 0 ? "1" : "0");
-        Console.WriteLine(sb);
-    }
-
-    public static void top()
-    {
-        sb.Append(sstack.Count == 0 ? "-1" : sstack[sstack.Count - 1]);
-        Console.WriteLine(sb);
-    }
-    
-    
-    
 }
+
+Console.WriteLine(sb.ToString());
