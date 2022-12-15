8299d60601d675079f2e2023134bf6ab5cc98bb0
Seo <108641066+wk-seo@users.noreply.github.com>
2022-12-15 19:35:18 +0900

    [Silver IV] Title: ATM, Time: 68 ms, Memory: 5676 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.cs
+++ 백준/Silver/11399. ATM/ATM.cs
@@ -1,17 +1,17 @@
+// [1] Input
 int n = int.Parse(Console.ReadLine());
-int[] inputs = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int[] p = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
 
-Array.Sort(inputs);
-//1, 2, 3, 3, 4
-int result = 0;
-int temp = 0;
-for(int i = 0; i<n; i++){
-    temp += inputs[i];
-        result += temp;
+// [2] Process
+int minTime = 0;
+int sumMinTime = 0;
+
+Array.Sort(p);
+for (int i = 0; i < p.Length; i++)
+{
+    minTime += p[i];
+    sumMinTime += minTime;
 }
-//i == 0, temp = 1, result  = 1
-//i == 1, temp = 1+2 = 3, result  = 4
-//i == 2, temp = 3+3 = 6, result  = 10
-//i == 3, temp = 6+3 = 9, result  = 19
-//i == 4, temp = 9+4 = 13, result  = 32
-Console.WriteLine(result);
+
+// [3] Output
+Console.WriteLine(sumMinTime);
